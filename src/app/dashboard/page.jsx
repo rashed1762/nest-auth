65415f3c54a73daa1b4999112b4f686a49d44eec
rashed <rashedul@dominno.local>
2024-10-16 12:50:06 +0600

    first commit

---
+++ src/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+import UserInfo from '@/components/Userinfo'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><UserInfo></UserInfo></div>
+  )
+}
+
+export default page
---
+++ src/app/register/page.jsx
@@ -0,0 +1,10 @@
+import RegisterForm from '@/components/Register'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><RegisterForm></RegisterForm></div>
+  )
+}
+
+export default page
